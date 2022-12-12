c0bf0942d55739f323f53cbdecec4533f5dab339
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-12 22:43:24 +0530

    Input

---
+++ Fundametals/com.ravi/FirstInput.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class First {
+       public static void main(String[] args) {
+        int a;
+        System.out.println("enter a number");
+        Scanner s=new Scanner(System.in);
+        
+        a=s.nextInt();
+        System.out.print("the number that u entered is ");
+         System.out.println(a)   ;
+       }
+            
+        }
