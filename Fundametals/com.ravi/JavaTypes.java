023035460f73d626cf10c14e935ee0cc853fea2d
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-14 22:53:43 +0530

    Javatyes

---
+++ Fundametals/com.ravi/JavaTypes.java
@@ -0,0 +1,16 @@
+package JavaGuide.Fundametals.com.ravi;
+
+public class JavaTypes {
+    //byte -1 short-2 int-4 long-8 float-4 char-2 double-8 boolean-1 
+    //primitiveTypes***
+    byte age =30;
+    int phonenumb=123456789;
+    long phonenumb2=1234567895554545L;
+    float piz=3.2544F;
+     char letter ='*';
+     boolean isanAdult=true;
+     //non-primitiveTypes***
+     String name ="ravi";
+     System.out.println(name.length());//to find length
+     String name1= new String("xccnohncioc");
+}
