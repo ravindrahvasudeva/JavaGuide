879a4140e838437460a9f29eeea793b1647e014f
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-16 23:06:03 +0530

    shotest height

--- Fundametals/com.ravi/InterviewCodes/MinimizetheHeights.java
+++ Fundametals/com.ravi/InterviewCodes/MinimizetheHeights.java
@@ -1,13 +1,4 @@
-/*Given an array arr[] denoting heights of N towers and a positive integer K.
 
-For each tower, you must perform exactly one of the following operations exactly once.
-
-Increase the height of the tower by K
-Decrease the height of the tower by K
-Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
-
-You can find a slight modification of the problem here.
-Note: It is compulsory to increase or decrease the height by K for each tower. After the operation, the resultant array should not contain any negative integers.*/
 class Solution 
 {
     int getMinDiff(int[] arr, int n, int k) 
