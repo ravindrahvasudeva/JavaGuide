fe9a0270036bc3bbb102c2371a3db088a8367b01
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-19 22:16:35 +0530

    Arrays

--- Fundametals/com.ravi/ArraysInJava.java
+++ Fundametals/com.ravi/ArraysInJava.java
@@ -10,4 +10,10 @@ public class ArraysInJava {
         System.out.println(marks[2]);
         // o/p=99.
 
-       
+        // now if u consider maeks is not declared then it will have a value to be '0'
+        // unlike c++ which has garbage value
+        // marks[0]=92;
+        // marks[1]=95; {if not declared}
+        // marks[3]=99;
+        System.out.println(marks);// o/p =0.
+
