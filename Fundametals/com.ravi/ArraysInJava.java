a2d1fa1cc770d1fbeb654068d9de2748551f623b
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-19 22:16:11 +0530

    Arrays

--- Fundametals/com.ravi/ArraysInJava.java
+++ Fundametals/com.ravi/ArraysInJava.java
@@ -3,4 +3,11 @@ public class ArraysInJava {
     public static void main(String args[]) {
         // consider if u want to enter student marks which of same data type u create a
         // list which can contain same datatypes = Array
-      
+        int[] marks = new int[3];
+        marks[0] = 95;
+        marks[1] = 92;
+        marks[2] = 99;
+        System.out.println(marks[2]);
+        // o/p=99.
+
+       
