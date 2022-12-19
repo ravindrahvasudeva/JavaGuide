ec7fa4b7a11023c45dd259cc4d84e3ba4bda0d85
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-19 22:15:46 +0530

    Arrays

---
+++ Fundametals/com.ravi/ArraysInJava.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+public class ArraysInJava {
+    public static void main(String args[]) {
+        // consider if u want to enter student marks which of same data type u create a
+        // list which can contain same datatypes = Array
+      
