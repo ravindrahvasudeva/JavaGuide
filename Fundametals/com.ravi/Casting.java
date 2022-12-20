116bcd81232aab36f6119dcb56a543b5167ba9e1
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-20 21:42:21 +0530

    type casting

--- Fundametals/com.ravi/Casting.java
+++ Fundametals/com.ravi/Casting.java
@@ -1,9 +1,27 @@
 public class Casting {
     public static void main(String[] args){
         //casting
+
+        //implicit casting
         double price = 100.00;
         double finalPrice = price + 18;//here 18 is an integer but there is no data loss{implicit conversion}   while converting int to double.
-        System.out.println(finalPrice);
+        System.out.println(finalPrice);//o/p=118
+        
+        //error
+        int p =100;
+        int fp =p+18.0; //error we cannot convert higher priority datatype to a lesser one there will be data loss
+
+        //explicit casting
+        int p =100;
+        int fp =p+(int)18.0;//we have converted double to int by  type casting
+
+
+
+
+
+        
+
+
     }
     
 }
