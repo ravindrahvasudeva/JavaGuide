f3dc74e7b70e7ae326ff0749abf86c8aa20f4ccd
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-20 18:00:15 +0530

    type casting

--- Fundametals/com.ravi/Casting.java
+++ Fundametals/com.ravi/Casting.java
@@ -1,6 +1,9 @@
 public class Casting {
     public static void main(String[] args){
         //casting
+        double price = 100.00;
+        double finalPrice = price + 18;
+        System.out.println(finalPrice);
     }
     
 }
