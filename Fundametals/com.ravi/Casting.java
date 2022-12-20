4c5434ed2827908f430b4df6c1ac0d9f3ad8d24f
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-20 18:03:06 +0530

    type casting

--- Fundametals/com.ravi/Casting.java
+++ Fundametals/com.ravi/Casting.java
@@ -2,7 +2,7 @@ public class Casting {
     public static void main(String[] args){
         //casting
         double price = 100.00;
-        double finalPrice = price + 18;
+        double finalPrice = price + 18;//here 18 is an integer but there is no data loss{implicit conversion}   while converting int to double.
         System.out.println(finalPrice);
     }
     
