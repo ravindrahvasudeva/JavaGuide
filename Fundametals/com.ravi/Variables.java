84ba6d72c866ab5401adce799ac5e5bfe5423806
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-21 21:24:49 +0530

    comments in java

---
+++ Fundametals/com.ravi/Variables.java
@@ -0,0 +1,9 @@
+
+
+public class Variables {
+    //variables
+    String name = "Ravi";//1st variable
+    int age =21;
+    String friend=name;//new variable directly gets assigned with the  1st varibles value.
+    System.out.println(name);//o/p since Strings are immutable the value cant be changed so we assign value to new variable.
+}
