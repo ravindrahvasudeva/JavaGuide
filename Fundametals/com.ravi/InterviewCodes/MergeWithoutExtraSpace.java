980d45364f333528eb4ca274abdedf829e934cd9
ravindrahv <ravindrahvasudeva@gmail.com>
2022-12-21 22:33:50 +0530

    MERGE

--- Fundametals/com.ravi/InterviewCodes/MergeWithoutExtraSpace.java
+++ Fundametals/com.ravi/InterviewCodes/MergeWithoutExtraSpace.java
@@ -1,3 +1,5 @@
+/*Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space.
+ Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.*/
 class Solution{
     public static void merge(long arr1[], long arr2[], int n, int m) 
     {
@@ -16,3 +18,13 @@ int i = 0, j = 0, k = n - 1;
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         }}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
